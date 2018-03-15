600705f9c7b4ff186a92c85404a61b907b19adc2
Vitali <vitali_zevako@epam.com>
2018-03-15 15:41:25 +0300

    added example to kill zombie job

--- jenkins/scripts.groovy
+++ jenkins/scripts.groovy
@@ -1,4 +1,4 @@
-!#groovy
+#!groovy
 
 #Stop jenkins zombie job
 Jenkins.instance.getItemByFullName("JobName").getBuildByNumber(JobNumber).finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"))
